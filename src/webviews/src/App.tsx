b3fdc5a8d1801c2ec4b6ed9699bf177a5470c06f
Idan Gazit <idan@gazit.me>
2021-03-17 16:54:19 -0700

    Gotta start somewhere

---
+++ src/webviews/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface AppProps {}
+
+function App({}: AppProps) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>It works!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
