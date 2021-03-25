c5953aac0ffb9440c58e0b0a12db7ccb789be7b8
Idan Gazit <idan@gazit.me>
2021-03-25 00:00:18 -0700

    Add proper typing for tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   purge: ['./src/webviews/**/*.{js,jsx,ts,tsx,css}'],
-
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
